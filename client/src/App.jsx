6aafec9d1ab0a914c4becc7c9921d79e74ee0c94
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-03-11 22:15:33 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Route, Routes, useMatch} from 'react-router-dom';
+import Home from './pages/student/Home';
+import CourseList from './pages/student/CourseList';
+import CourseDetails from './pages/student/CourseDetails';
+import MyEnrollments from './pages/student/MyEnrollments';
+import Player from './pages/student/Player';
+import Loading from './components/student/Loading';
+import Educator from './pages/educator/Educator';
+import Dashboard from './pages/educator/Dashboard';
+import AddCourse from './pages/educator/AddCourse';
+import MyCourses from './pages/educator/MyCourses';
+import StudentsEnrolled from './pages/educator/StudentsEnrolled';
+import Navbar from './components/student/Navbar';
+
+
+const App = () => {
+
+  // if we are on the educator route or the educator is using the website.
+  const isEducatorRoute = useMatch('/educator/*')
+
+  return (
+    <div className='text-default min-h-screen bg-white'>
+        {/*If we are not on the educator route (if the user is not an educator) then only display the navbar*/}
+        {!isEducatorRoute && <Navbar/>}
+        
+        {/*Setting up the Home Page Route, or pages*/}
+        <Routes>
+            <Route path = '/' element = {<Home/>}/>
+            <Route path = '/course-list' element = {<CourseList/>}/>
+            <Route path = '/course-list/:input' element = {<CourseList/>}/>
+            <Route path = '/course/:id' element = {<CourseDetails/>}/>
+            <Route path = '/my-enrollments' element = {<MyEnrollments/>}/>
+            <Route path = '/player/:courseId' element = {<Player/>}/>
+            <Route path = '/loading/:path' element = {<Loading/>}/>
+
+            {/*Routes inside educator routes*/}
+            <Route path = '/educator' element = {<Educator/>}>
+                <Route path = 'educator' element = {<Dashboard/>}/>     
+                <Route path = 'add-course' element = {<AddCourse/>}/>     
+                <Route path = 'my-courses' element = {<MyCourses/>}/>     
+                <Route path = 'student-enrolled' element = {<StudentsEnrolled/>}/>     
+            </Route>
+
+
+        </Routes>
+    </div>
+  )
+}
+
+export default App
